b6a46f11e6043d1e2afdaf989f0e2076fa734fe9
IBrickhouse <brickhoi@oregonstate.edu>
2022-02-20 01:57:51 -0800

    Add functionality to get url, open, and save picture,
    Gets reddit url, displays images, and saves to project directory. overwrites picture file each run.

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+#import sys
 from RedditReader import Subreddit
+from PIL import Image
+import requests
 
+# gets the url from reddit. This uses r/twicemedia
 image = Subreddit("Twicemedia")
 image.get_random()
-url = image.url
-print(url)
+image_url = image.url
+#url.show()
+#print(image_url)
+
+# grabs the image and shows it
+im = Image.open(requests.get(image_url, stream=True).raw)
+im.show()
+
+#saves image to project directory
+im.save(images/"twicebg.jpg")
